d41f3976f6b9cc0971a0ded639c6bb0dc1088e73
diogo lima <diogolm.nascimento@gmail.com>
2021-09-16 14:30:03 -0300

    index.js alterado

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let dataDoEvento = [25, 11, 2021];
 let dataDeHoje = [20, 11, 2021];
-let idade = 22;
+let idade = 17;
 let quantidadeDePessoas = 100;
 
 if((dataDoEvento[2] - dataDeHoje[2]) < 0){
@@ -9,7 +9,7 @@ if((dataDoEvento[2] - dataDeHoje[2]) < 0){
     console.log("Impossível marcar, pois o MÊS de hoje é posterior ao churrasco!!");
 } else if ((dataDoEvento[0] - dataDeHoje[0]) < 0){
     console.log("Impossível marcar, pois o DIA de hoje é posterior ao churrasco!");
-} else if(idade > 18){
+} else if (idade > 18){
     console.log("Entrada permitida, Maior de Idade");
 } else {
     console.log("Entrada NÃO permitida, Menor de Idade");
