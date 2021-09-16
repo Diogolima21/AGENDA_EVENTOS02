74af12f23bfda97f8db9b22a54769147fefe0784
diogo lima <diogolm.nascimento@gmail.com>
2021-09-16 20:44:24 -0300

    alterado index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let dataDoEvento = [25, 11, 2021];
-let dataDeHoje = [20, 11, 2021];
-let idade = 17;
+let dataDeHoje = [16, 09, 2021];
+let idade = 19;
 let quantidadeDePessoas = 100;
 
 if((dataDoEvento[2] - dataDeHoje[2]) < 0){
@@ -9,13 +9,14 @@ if((dataDoEvento[2] - dataDeHoje[2]) < 0){
     console.log("Impossível marcar, pois o MÊS de hoje é posterior ao churrasco!!");
 } else if ((dataDoEvento[0] - dataDeHoje[0]) < 0){
     console.log("Impossível marcar, pois o DIA de hoje é posterior ao churrasco!");
-} else if (idade > 18){
-    console.log("Entrada permitida, Maior de Idade");
 } else {
-    console.log("Entrada NÃO permitida, Menor de Idade");
-} 
-if(quantidadeDePessoas <=100 ){
-    console.log("Evento marcado com suceso!!");
-} else {
-    console.log("Evento NÃO permitido, ultrapasoou o limite de pessoas");
-} 
+    if (idade <18){
+         console.log("Entrada NÃO permitida, Menor de Idade");  
+        } else {
+            if(quantidadeDePessoas > 100){
+                console.log("Evento NÃO permitido, ultrapasoou o limite de pessoas");
+        } else {
+            console.log("Evento marcado com suceso!!");
+        }
+    }        
+}    
