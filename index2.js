25314b1c5dc81b39827066a5f16b2827a0605a32
diogo lima <diogolm.nascimento@gmail.com>
2021-09-20 10:20:39 -0300

    codigo lista de alunos

---
+++ index2.js
@@ -0,0 +1,17 @@
+/*
+let num = 0;
+
+while(num < 10){
+    console.log(num)
+    num++  //num = num + 1;
+}
+*/
+
+let listaNomes =["Ana", "Beto", "Clara"];
+//console.log(listaNomes.length);
+//console.log(listaNomes[1].length);
+
+
+for(let contador= 0; contador < listaNoems.length; contador++){
+    console.log(listaNomes[contador])
+}
